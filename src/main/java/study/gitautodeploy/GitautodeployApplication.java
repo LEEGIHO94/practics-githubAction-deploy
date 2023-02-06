f455fcb1889b6a981455b3eb858c05eca7ee8399
LEEGIHO <lgh941107@gmil.com>
2023-02-06 14:00:46 +0900

    deployTest

---
+++ src/main/java/study/gitautodeploy/GitautodeployApplication.java
@@ -0,0 +1,13 @@
+package study.gitautodeploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitautodeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitautodeployApplication.class, args);
+	}
+
+}
---
+++ src/test/java/study/gitautodeploy/GitautodeployApplicationTests.java
@@ -0,0 +1,13 @@
+package study.gitautodeploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitautodeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
